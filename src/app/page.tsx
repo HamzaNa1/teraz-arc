e4cd6be4b975e12b1720bedbdcb6593e057c988f
HamzaN12 <hamzanasab4@gmail.com>
2023-08-01 14:51:36 +0300

    Added components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Contact from "@/components/Contact";
+import Goals from "@/components/Goals";
+import Main from "@/components/Main";
 import Navbar from "@/components/Navbar";
+import Portfolio from "@/components/Portfolio";
+import Services from "@/components/Services";
+import Vision from "@/components/Vision";
 
 export default function Home() {
-  return (
-    <main><Navbar/></main>
-  )
+	return (
+		<main>
+			<Navbar />
+			<Main />
+			<Vision />
+			<Goals />
+			<Services />
+			<Portfolio />
+			<Contact />
+		</main>
+	);
 }
