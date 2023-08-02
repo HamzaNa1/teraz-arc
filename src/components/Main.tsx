0d0ca4120197b606abef86de9e820488d22cf4f6
Zakarya-Khawnda <111238130+Zakarya-Khawnda@users.noreply.github.com>
2023-08-02 15:39:41 +0300

    ..

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,8 @@
+
+
 export default function Main() {
-	return <div className="w-screen"></div>;
+	return <div className="w-screen h-fit bg-[#323232]">
+		<h1>hi</h1>
+		<img src='../../public/images/Group2.svg' alt='' className=''></img>
+	</div>;
 }
