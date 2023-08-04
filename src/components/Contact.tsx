6a88450ffbb92f64707051d73af5bdfb28cf22d4
HamzaN12 <136239172+HamzaN12@users.noreply.github.com>
2023-08-04 15:21:11 +0300

    Cleanup contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,47 +1,43 @@
-import { PiHouseFill } from "react-icons/pi";
-
-// PiHouseLine
 export default function Contact() {
-  return (
-    <div className="container max-w-full h-[1024px] bg-contact">
-      <div className="h-full flex flex-col justify-center items-center gap-20">
-        <div className="w-[400px] h-[400px]">
-          <img src="/images/icon.svg" alt="" className="w-full h-full" />
-        </div>
+	return (
+		<div className="container max-w-full h-[1024px] bg-contact">
+			<div className="h-full flex flex-col justify-center items-center gap-20">
+				<div className="w-[400px] h-[400px]">
+					<img src="/images/icon.svg" alt="" className="w-full h-full" />
+				</div>
 
-        <div className="flex flex-col gap-5 text-center">
-          <p className=" text-4xl text-black ">
-            {" "}
-            بكل تأكيد هناك المزيد من الأعمال والتفاصيل{" "}
-          </p>
-          <p className="text-4xl text-black justify-center">
-            لا تتردد بالتواصل معنا
-          </p>
-        </div>
+				<div className="flex flex-col gap-5 text-center">
+					<p className=" text-4xl text-black">
+						بكل تأكيد هناك المزيد من الأعمال والتفاصيل
+					</p>
+					<p className="text-4xl text-black justify-center">
+						لا تتردد بالتواصل معنا
+					</p>
+				</div>
 
-        <div>
-          <div className="grid grid-cols-2 gap-x-20 gap-y-24  ">
-            <div className="flex flex-row gap-3 justify-center items-center	">
-              <img src="/images/home.svg  "></img>
-              <span className="text-black text-4xl   ">Saudi Arabia</span>
-            </div>
-            <div className="flex flex-row gap-3 justify-center items-center	">
-              <img src="/images/call.svg  "></img>
-              <span className="text-black text-4xl   ">0504518753</span>
-            </div>
-            <div className="flex flex-row gap-3 justify-center items-center	">
-              <img src="/images/mail.svg  "></img>
-              <span className="text-black text-3xl text-justify   ">
-                info@terazart.com
-              </span>
-            </div>
-            <div className="flex flex-row gap-3 justify-center items-center	">
-              <img src="/images/call.svg  "></img>
-              <span className="text-black text-4xl   ">011-470098</span>
-            </div>{" "}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				<div>
+					<div className="grid grid-cols-2 gap-x-20 gap-y-24">
+						<div className="flex flex-row gap-3 justify-center items-center">
+							<img src="/images/home.svg"></img>
+							<span className="text-black text-4xl">Saudi Arabia</span>
+						</div>
+						<div className="flex flex-row gap-3 justify-center items-center	">
+							<img src="/images/call.svg"></img>
+							<span className="text-black text-4xl">0504518753</span>
+						</div>
+						<div className="flex flex-row gap-3 justify-center items-center">
+							<img src="/images/mail.svg"></img>
+							<span className="text-black text-3xl text-justify">
+								info@terazart.com
+							</span>
+						</div>
+						<div className="flex flex-row gap-3 justify-center items-center">
+							<img src="/images/call.svg"></img>
+							<span className="text-black text-4xl">011-470098</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
