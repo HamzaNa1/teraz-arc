1e2cdf6aa855fedcf41af7bfa4ee419c79a257cd
HamzaN12 <136239172+HamzaN12@users.noreply.github.com>
2023-08-04 15:15:28 +0300

    Fixed main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Contact from "@/components/Contact";
 import Goals from "@/components/Goals";
 import Main from "@/components/Main";
-import Navbar from "@/components/Navbar";
 import Portfolio from "@/components/Portfolio";
 import Services from "@/components/Services";
 import Vision from "@/components/Vision";
@@ -9,7 +8,6 @@ import Vision from "@/components/Vision";
 export default function Home() {
 	return (
 		<main className="w-screen">
-			<Navbar />
 			<Main />
 			<Vision />
 			<Goals />
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,35 +1,38 @@
+import Navbar from "./Navbar";
+
 export default function Main() {
 	return (
-		<div className="container max-w-full h-[1024px] bg-[#323232]">
+		<div className="container flex flex-col max-w-full h-screen bg-[#323232]">
+			<Navbar></Navbar>
 			<div className="relative w-full h-full">
 				<div className="absolute w-full h-full">
-					<div className="w-full flex justify-start">
-						<img src="/images/v1.svg"></img>
+					<div className="flex flex-wrap w-full h-full">
+						<div className="w-[75%] h-[50%]">
+							<img src="/images/v1.svg"></img>
+						</div>
+						<div className="w-[25%] h-[50%]">
+							<img src="/images/v2.svg"></img>
+						</div>
+						<div className="h-[50%] w-[40%] flex items-end">
+							<img src="/images/v4.svg"></img>
+						</div>
 					</div>
 				</div>
 				<div className="absolute w-full h-full">
-					<div className="w-full flex justify-end">
-						<img src="/images/v2.svg"></img>
-					</div>
-				</div>
-				<div className="absolute w-full h-full">
-					<div className="w-full h-full flex">
-						<img src="/images/v4.svg" className="self-end"></img>
-					</div>
-				</div>
-				<div className="absolute w-full h-full">
-					<div className="w-full h-full flex">
-						<img src="/images/v3.svg" className="self-end w-full"></img>
+					<div className="h-full ww-full flex items-end">
+						<img src="/images/v3.svg"></img>
 					</div>
 				</div>
 
 				<div className="w-full h-full flex flex-col justify-center items-center">
-					<img src="/images/icon.svg" alt="" className="w-[50vh]" />
+					<div className="w-[25%] max-md:w-[50%]">
+						<img src="/images/icon.svg" alt="" className="w-full" />
+					</div>
 					<div className="flex flex-col gap-5 text-center mt-5">
-						<p className="text-primary text-5xl">
+						<p className="text-primary text-5xl max-md:text-3xl">
 							مؤسسة الطراز المعماري للمقاولات
 						</p>
-						<p className="text-3xl">مستمرون معكم منذ 1993م</p>
+						<p className="text-3xl max-md:text-xl">مستمرون معكم منذ 1993م</p>
 					</div>
 				</div>
 			</div>
