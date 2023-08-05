b3c0fb8c121896cb9042320003bdaa6c344ce5c4
HamzaN12 <136239172+HamzaN12@users.noreply.github.com>
2023-08-05 20:17:17 +0300

    Update Goals.tsx

--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -1,7 +1,7 @@
-import { HiWrenchScrewdriver } from "react-icons/hi2";
-import { RiMedalFill } from "react-icons/ri";
+import { RiMedalLine } from "react-icons/ri";
 import { MdOutlineShoppingCart, MdOutlineHandshake } from "react-icons/md";
 import { IoMdStopwatch } from "react-icons/io";
+import { LiaWrenchSolid } from "react-icons/lia";
 
 export default function Goals() {
 	return (
@@ -9,12 +9,12 @@ export default function Goals() {
 			<span className="text-[rgb(81,81,81)] text-4xl">أهداف المؤسسة</span>
 			<div className=" flex gap-32">
 				<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center">
-					<HiWrenchScrewdriver className="fill-primary h-[40%] w-full p-5" />
+					<LiaWrenchSolid className="fill-primary h-[40%] w-full p-5" />
 					<span className="text-secondary">تنفيذ الأعمال</span>
 					<span className="text-secondary">بأحدث التقنيات</span>
 				</div>
 				<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center">
-					<RiMedalFill className="fill-primary h-[40%] w-full p-5" />
+					<RiMedalLine className="fill-primary h-[40%] w-full p-5" />
 					<span className="text-secondary">ضمان جودة</span>
 					<span className="text-secondary">الأعمال</span>
 				</div>
