eda6c06ddb8bc61abaac83d5a26f7129df9c2821
HamzaN12 <136239172+HamzaN12@users.noreply.github.com>
2023-08-05 19:36:23 +0300

    Add hamburger

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import { GiHamburgerMenu } from "react-icons/gi";
+
 export default function Navbar() {
 	return (
 		<>
 			<div className="container max-w-full h-[5rem] bg-white">
 				<div className="w-full h-full flex justify-end">
-					<div className="invisible md:visible hidden md:flex w-full justify-center items-center text-primary text-3xl">
+					<div className="hidden md:flex w-full justify-center items-center text-primary text-3xl">
 						<button className="m-auto">تواصل</button>
 						<button className="m-auto">أعمالنا</button>
 						<button className="m-auto">خدماتنا</button>
 						<button className="m-auto">أهدافنا</button>
 						<button className="m-auto">رؤيتنا</button>
+					</div>
+					<div className="md:hidden mr-auto">
+						<GiHamburgerMenu className="fill-primary h-full w-full p-4" />
 					</div>
 
 					<div className="h-full p-2">
