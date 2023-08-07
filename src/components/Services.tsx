91146e0658f27b5e5d2bee56a76411c8eec4efcc
Zakarya-Khawnda <111238130+Zakarya-Khawnda@users.noreply.github.com>
2023-08-07 15:53:51 +0300

    Navbar edit

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,36 +1,38 @@
 import { MdVerified } from "react-icons/md";
 
 export default function Services() {
-	return (
-		<div className="container max-w-full h-[1024px] bg-services flex flex-row justify-center items-center gap-x-72">
-			<div className="rounded-2xl border-[#BF971B] border-[4px] p-2">
-				<img
-					src="/images/bad9454e-a349-441c-848f-486d21eed8ca.jpg"
-					alt=""
-					className="drop-shadow-2xl rounded-2xl object-contain"
-				/>
-			</div>
-			<div className="flex flex-col gap-y-10  text-primary text-5xl ">
-				<div className="flex flex-row gap-x-10 justify-end items-center  ">
-					<span>دراسة التصاميم و تقديم حلول</span>
-					<MdVerified className=" fill-primary w-36 h-36 " />
-				</div>
+  return (
+    <div className="container max-w-full h-[1024px] bg-services flex flex-row justify-center items-center gap-x-20">
+      <div className="drop-shadow-2xl">
+        <Image
+          src="/images/bad9454e-a349-441c-848f-486d21eed8ca.jpg"
+          alt=" "
+          width={900}
+          height={900}
+          className="rounded-2xl"
+        />
+      </div>
+      <div className="flex flex-col gap-y-10  text-primary text-6xl">
+        <div className="flex flex-row gap-x-10 justify-center items-center  ">
+          <span>دراسة التصاميم و تقديم حلول</span>
+          <MdVerified className=" fill-primary w-36 h-36 " />
+        </div>
 
-				<div className="flex flex-row gap-x-10 justify-end items-center  ">
-					<span>أعمال ترميم واعادة تأهيل</span>
-					<MdVerified className=" fill-primary w-36 h-36  " />
-				</div>
+        <div className="flex flex-row gap-x-10   justify-center items-center  ">
+          <span>دراسة التصاميم و تقديم حلول</span>
+          <MdVerified className=" fill-primary w-36 h-36  " />
+        </div>
 
-				<div className="flex flex-row gap-x-10 justify-end items-center  ">
-					<span>أعمال الديكور والتشطيبات</span>
-					<MdVerified className=" fill-primary w-36 h-36 " />
-				</div>
+        <div className="flex flex-row gap-x-10  justify-center items-center  ">
+          <span>دراسة التصاميم و تقديم حلول</span>
+          <MdVerified className=" fill-primary w-36 h-36 " />
+        </div>
 
-				<div className="flex flex-row gap-x-10 justify-end items-center ">
-					<span> أعمال البناء والتشييد</span>
-					<MdVerified className=" fill-primary w-36 h-36  " />
-				</div>
-			</div>
-		</div>
-	);
+        <div className="flex flex-row gap-x-10  justify-center items-center ">
+          <span>دراسة التصاميم و تقديم حلول</span>
+          <MdVerified className=" fill-primary w-36 h-36  " />
+        </div>
+      </div>
+    </div>
+  );
 }
