7389b84ca00ce0efb7ba823ace13849a62524e6f
HamzaN12 <136239172+HamzaN12@users.noreply.github.com>
2023-08-07 16:05:06 +0300

    Revert "Navbar edit"
    This reverts commit 91146e0658f27b5e5d2bee56a76411c8eec4efcc.

--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -5,59 +5,51 @@ import { LiaWrenchSolid } from "react-icons/lia";
 
 export default function Goals() {
   return (
-    <section id="Goals">
-      <div className="container max-w-full h-[1024px] bg-[#EEEEEE] flex flex-col justify-center items-center gap-y-11">
-        <span className="text-[rgb(81,81,81)] text-4xl">أهداف المؤسسة</span>
-        <div className=" flex gap-32">
-          <div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
-            <LiaWrenchSolid className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
-            <span className="text-secondary group-hover:text-white">
-              تنفيذ الأعمال
-            </span>
-            <span className="text-secondary group-hover:text-white">
-              بأحدث التقنيات
-            </span>
-          </div>
-          <div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
-            <RiMedalLine className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
-            <span className="text-secondary group-hover:text-white">
-              ضمان جودة
-            </span>
-            <span className="text-secondary group-hover:text-white">
-              الأعمال
-            </span>
-          </div>
-          <div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
-            <MdOutlineShoppingCart className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
-            <span className="text-secondary group-hover:text-white">
-              انتقاء أفضل
-            </span>
-            <span className="text-secondary group-hover:text-white">
-              المواد
-            </span>
-          </div>
+    <div className="container max-w-full h-[1024px] bg-[#EEEEEE] flex flex-col justify-center items-center gap-y-11">
+      <span className="text-[rgb(81,81,81)] text-4xl">أهداف المؤسسة</span>
+      <div className=" flex gap-32">
+        <div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
+          <LiaWrenchSolid className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
+          <span className="text-secondary group-hover:text-white">
+            تنفيذ الأعمال
+          </span>
+          <span className="text-secondary group-hover:text-white">
+            بأحدث التقنيات
+          </span>
         </div>
-        <div className="flex gap-32">
-          <div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
-            <MdOutlineHandshake className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
-            <span className="text-secondary group-hover:text-white">
-              بناء الثقة
-            </span>
-            <span className="text-secondary group-hover:text-white">
-              بين العملاء
-            </span>
-          </div>
-          <div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
-            <IoMdStopwatch className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
-            <span className="text-secondary group-hover:text-white">
-              توفير الوقت
-            </span>
-            <span className="text-secondary group-hover:text-white">
-              و الجهد
-            </span>
-          </div>
+        <div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
+          <RiMedalLine className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
+          <span className="text-secondary group-hover:text-white">
+            ضمان جودة
+          </span>
+          <span className="text-secondary group-hover:text-white">الأعمال</span>
+        </div>
+        <div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
+          <MdOutlineShoppingCart className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
+          <span className="text-secondary group-hover:text-white">
+            انتقاء أفضل
+          </span>
+          <span className="text-secondary group-hover:text-white">المواد</span>
         </div>
       </div>
-    </section>
+      <div className="flex gap-32">
+        <div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
+          <MdOutlineHandshake className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
+          <span className="text-secondary group-hover:text-white">
+            بناء الثقة
+          </span>
+          <span className="text-secondary group-hover:text-white">
+            بين العملاء
+          </span>
+        </div>
+        <div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
+          <IoMdStopwatch className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
+          <span className="text-secondary group-hover:text-white">
+            توفير الوقت
+          </span>
+          <span className="text-secondary group-hover:text-white">و الجهد</span>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,65 +1,32 @@
 "use client";
-import { useState } from "react";
 
 import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
-  const [activeNav, setActiveNav] = useState("#");
+	return (
+		<>
+			<div className="container max-w-full h-[5rem] bg-white">
+				<div className="w-full h-full flex justify-end">
+					<div className="hidden md:flex w-full justify-center items-center text-primary text-3xl">
+						<button className="m-auto">تواصل</button>
+						<button className="m-auto">أعمالنا</button>
+						<button className="m-auto">خدماتنا</button>
+						<button className="m-auto">أهدافنا</button>
+						<button className="m-auto">رؤيتنا</button>
+					</div>
+					<div className="md:hidden mr-auto">
+						<GiHamburgerMenu className="fill-primary h-full w-full p-4" />
+					</div>
 
-  return (
-    <>
-      <div className="container max-w-full h-[5rem] bg-white">
-        <div className="w-full h-full flex justify-end">
-          <div className="hidden md:flex w-full justify-center items-center text-primary text-3xl">
-            <a
-              href="#Contact"
-              onClick={() => setActiveNav("#Contact")}
-              className={activeNav === "#Contact" ? "active" : ""}
-            >
-              تواصل
-            </a>
-            <a
-              href="#Work"
-              onClick={() => setActiveNav("#Work")}
-              className={activeNav === "#Work" ? "active" : ""}
-            >
-              أعمالنا
-            </a>
-            <a
-              href="#Services"
-              onClick={() => setActiveNav("#Services")}
-              className={activeNav === "#Services" ? "active" : ""}
-            >
-              خدماتنا
-            </a>
-            <a
-              href="#Goals"
-              onClick={() => setActiveNav("#Goals")}
-              className={activeNav === "#Goals" ? "active" : ""}
-            >
-              أهدافنا
-            </a>
-            <a
-              href="#Vision"
-              onClick={() => setActiveNav("#Vision")}
-              className={activeNav === "#Vision" ? "active" : ""}
-            >
-              رؤيتنا
-            </a>
-          </div>
-          <div className="md:hidden mr-auto">
-            <GiHamburgerMenu className="fill-primary h-full w-full p-4" />
-          </div>
-
-          <div className="h-full p-2">
-            <img
-              src="/images/icon.svg"
-              alt=""
-              className="object-contain w-full h-full"
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+					<div className="h-full p-2">
+						<img
+							src="/images/icon.svg"
+							alt=""
+							className="object-contain w-full h-full"
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
