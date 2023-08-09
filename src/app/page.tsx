d3fc58d2b8891b3551499ad5d9e7daa22e48a2bd
HamzaN12 <hamzanasab4@gmail.com>
2023-08-09 14:08:33 +0300

    Added go to top button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,25 @@ import Main from "@/components/Main";
 import Portfolio from "@/components/Portfolio";
 import Services from "@/components/Services";
 import Vision from "@/components/Vision";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 export default function Home() {
+	const [scrollPosition, setScrollPosition] = useState(0);
+	const handleScroll = () => {
+		const position = window.scrollY;
+		setScrollPosition(position);
+	};
+	console.log(scrollPosition);
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll, { passive: true });
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	const vision = useRef<HTMLDivElement>(null);
 	const goals = useRef<HTMLDivElement>(null);
 	const services = useRef<HTMLDivElement>(null);
@@ -35,6 +51,16 @@ export default function Home() {
 			<div ref={contact}>
 				<Contact />
 			</div>
+			{scrollPosition != 0 ? (
+				<div
+					className="fixed bottom-5 right-5 cursor-pointer"
+					onClick={() => scrollTo({ top: 0, left: 0, behavior: "smooth" })}
+				>
+					<BsFillArrowUpCircleFill className="fill-primary w-10 h-10 drop-shadow-md hover:fill-darkPrimary active:fill-darkPrimary transition duration-300" />
+				</div>
+			) : (
+				<></>
+			)}
 		</main>
 	);
 }
