3a06e3306ba24cdc54663a86debdbcbe29be9910
HamzaN12 <hamzanasab4@gmail.com>
2023-08-10 12:25:36 +0300

    Added stars

--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -5,18 +5,92 @@ import { LiaWrenchSolid } from "react-icons/lia";
 
 export default function Goals() {
 	return (
-		<div className="container relative max-w-full min-h-fit h-fit bg-primaryPage flex flex-col justify-start items-center gap-y-11 p-10">
-			<div className="absolute w-full h-full">
+		<div className="container max-w-full min-h-[85vh] bg-primaryPage">
+			<div className="absolute w-full min-h-[85vh]">
 				{/* stars */}
 				<img
 					src="/images/whitestar.svg"
-					className="w-16 h-16 absolute top-[340px] left-[60px] rotate-45"
+					className="hidden md:block w-16 h-16 absolute top-[40px] left-[60px] rotate-45 drop-shadow-lg"
+				></img>
+
+				<img
+					src="/images/whitestar.svg"
+					className="hidden md:block w-16 h-16 absolute top-[120px] left-[96px] drop-shadow-lg"
+				></img>
+
+				<img
+					src="/images/whitestar.svg"
+					className="hidden md:block w-16 h-16 absolute bottom-[40px] right-[60px] drop-shadow-lg"
+				></img>
+
+				<img
+					src="/images/whitestar.svg"
+					className="hidden md:block w-16 h-16 absolute bottom-[120px] right-[96px] rotate-45 drop-shadow-lg"
+				></img>
+
+				<img
+					src="/images/whitestar.svg"
+					className="hidden md:block w-16 h-16 absolute bottom-[200px] right-[60px] drop-shadow-lg"
 				></img>
 			</div>
 
-			<span className="text-[rgb(81,81,81)] text-4xl z-10">أهداف المؤسسة</span>
-			<div className="h-full hidden md:flex flex-col justify-center items-center z-10">
-				<div className="flex gap-[6rem]">
+			<div className="flex flex-col justify-start items-center gap-y-12 p-10">
+				<span className="text-[rgb(81,81,81)] text-4xl z-10">
+					أهداف المؤسسة
+				</span>
+				<div className="w-full min-h-full hidden md:flex flex-col justify-center items-center z-10 flex-1">
+					<div className="flex gap-[6rem]">
+						<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
+							<LiaWrenchSolid className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
+							<span className="text-secondary group-hover:text-white">
+								تنفيذ الأعمال
+							</span>
+							<span className="text-secondary group-hover:text-white">
+								بأحدث التقنيات
+							</span>
+						</div>
+						<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
+							<RiMedalLine className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
+							<span className="text-secondary group-hover:text-white">
+								ضمان جودة
+							</span>
+							<span className="text-secondary group-hover:text-white">
+								الأعمال
+							</span>
+						</div>
+						<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
+							<MdOutlineShoppingCart className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
+							<span className="text-secondary group-hover:text-white">
+								انتقاء أفضل
+							</span>
+							<span className="text-secondary group-hover:text-white">
+								المواد
+							</span>
+						</div>
+					</div>
+					<div className="flex gap-[6rem]">
+						<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
+							<MdOutlineHandshake className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
+							<span className="text-secondary group-hover:text-white">
+								بناء الثقة
+							</span>
+							<span className="text-secondary group-hover:text-white">
+								بين العملاء
+							</span>
+						</div>
+						<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
+							<IoMdStopwatch className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
+							<span className="text-secondary group-hover:text-white">
+								توفير الوقت
+							</span>
+							<span className="text-secondary group-hover:text-white">
+								و الجهد
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div className="md:hidden h-fit w-full min-h-fit flex flex-col gap-8 justify-center items-center">
 					<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
 						<LiaWrenchSolid className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
 						<span className="text-secondary group-hover:text-white">
@@ -44,8 +118,6 @@ export default function Goals() {
 							المواد
 						</span>
 					</div>
-				</div>
-				<div className="flex gap-[6rem]">
 					<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
 						<MdOutlineHandshake className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
 						<span className="text-secondary group-hover:text-white">
@@ -64,48 +136,6 @@ export default function Goals() {
 							و الجهد
 						</span>
 					</div>
-				</div>
-			</div>
-
-			<div className="md:hidden h-fit w-full min-h-fit flex flex-col gap-8 justify-center items-center">
-				<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
-					<LiaWrenchSolid className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
-					<span className="text-secondary group-hover:text-white">
-						تنفيذ الأعمال
-					</span>
-					<span className="text-secondary group-hover:text-white">
-						بأحدث التقنيات
-					</span>
-				</div>
-				<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
-					<RiMedalLine className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
-					<span className="text-secondary group-hover:text-white">
-						ضمان جودة
-					</span>
-					<span className="text-secondary group-hover:text-white">الأعمال</span>
-				</div>
-				<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
-					<MdOutlineShoppingCart className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
-					<span className="text-secondary group-hover:text-white">
-						انتقاء أفضل
-					</span>
-					<span className="text-secondary group-hover:text-white">المواد</span>
-				</div>
-				<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
-					<MdOutlineHandshake className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
-					<span className="text-secondary group-hover:text-white">
-						بناء الثقة
-					</span>
-					<span className="text-secondary group-hover:text-white">
-						بين العملاء
-					</span>
-				</div>
-				<div className="h-[250px] w-[185px] border-[#BF971B] border-[4px] rounded-[40px] flex flex-col justify-center items-center group hover:bg-primary">
-					<IoMdStopwatch className="fill-primary h-[40%] w-full p-5 group-hover:fill-white" />
-					<span className="text-secondary group-hover:text-white">
-						توفير الوقت
-					</span>
-					<span className="text-secondary group-hover:text-white">و الجهد</span>
 				</div>
 			</div>
 		</div>
