caf7ba3d39d975a7efe19d76e84f1ca2b70822ae
HamzaN12 <hamzanasab4@gmail.com>
2023-10-21 15:14:18 +0300

    Moved google analytics to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Cairo } from "next/font/google";
-import Script from "next/script";
 
 const inter = Cairo({ subsets: ["arabic", "latin"] });
 
@@ -17,16 +16,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<Script src="https://www.googletagmanager.com/gtag/js?id=G-BRZVEE1FND" />
-			<Script id="google-analytics">
-				{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-BRZVEE1FND');
-        `}
-			</Script>
 			<body className={inter.className + " no-scrollbar"}>{children}</body>
 		</html>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Services from "@/components/Services";
 import Vision from "@/components/Vision";
 import { useEffect, useRef, useState } from "react";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
+import Script from "next/script";
 
 export default function Home() {
 	const [scrollPosition, setScrollPosition] = useState(0);
@@ -33,33 +34,45 @@ export default function Home() {
 	const elements = [vision, goals, services, portfolio, contact];
 
 	return (
-		<main className="w-screen max-w-[100vw] overflow-hidden">
-			<Main elements={elements} />
-			<div ref={vision}>
-				<Vision />
-			</div>
-			<div ref={goals}>
-				<Goals />
-			</div>
-			<div ref={services}>
-				<Services />
-			</div>
-			<div ref={portfolio}>
-				<Portfolio />
-			</div>
-			<div ref={contact}>
-				<Contact />
-			</div>
-			{scrollPosition != 0 ? (
-				<div
-					className="fixed bottom-5 right-5 cursor-pointer z-50"
-					onClick={() => scrollTo({ top: 0, left: 0, behavior: "smooth" })}
-				>
-					<BsFillArrowUpCircleFill className="fill-primary w-10 h-10 drop-shadow-md hover:fill-darkPrimary active:fill-darkPrimary transition duration-300" />
+		<>
+			<Script src="https://www.googletagmanager.com/gtag/js?id=G-BRZVEE1FND" />
+			<Script id="google-analytics">
+				{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-BRZVEE1FND');
+        `}
+			</Script>{" "}
+			<main className="w-screen max-w-[100vw] overflow-hidden">
+				<Main elements={elements} />
+				<div ref={vision}>
+					<Vision />
 				</div>
-			) : (
-				<></>
-			)}
-		</main>
+				<div ref={goals}>
+					<Goals />
+				</div>
+				<div ref={services}>
+					<Services />
+				</div>
+				<div ref={portfolio}>
+					<Portfolio />
+				</div>
+				<div ref={contact}>
+					<Contact />
+				</div>
+				{scrollPosition != 0 ? (
+					<div
+						className="fixed bottom-5 right-5 cursor-pointer z-50"
+						onClick={() => scrollTo({ top: 0, left: 0, behavior: "smooth" })}
+					>
+						<BsFillArrowUpCircleFill className="fill-primary w-10 h-10 drop-shadow-md hover:fill-darkPrimary active:fill-darkPrimary transition duration-300" />
+					</div>
+				) : (
+					<></>
+				)}
+			</main>
+		</>
 	);
 }
